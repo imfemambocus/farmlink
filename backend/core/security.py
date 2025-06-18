d69821655eeff74e619b2be10ae187df55e01778
Isfaaq M. F. Emambocus <imfemambocus@gmail.com>
2025-06-18 01:27:50 +0100

    Added env secret key for jwt

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,3 +1,4 @@
+import os
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
@@ -6,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session, joinedload
 from models.user import User
 
-SECRET_KEY = "your_super_secret_key"
+SECRET_KEY = os.getenv("SECRET_KEY", "your_super_secret_key_for_development_only")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 1 day
 
