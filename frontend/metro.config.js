5f1d180eb2bf013fd907e75d8e442fa3f1b325d4
Isfaaq M. F. Emambocus <imfemambocus@gmail.com>
2025-07-03 20:39:34 +0100

    Reverted metro config to initial state

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,11 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-config.resolver = config.resolver || {};
-config.resolver.alias = {
-    '@react-native-voice/voice': require.resolve('@react-native-voice/voice'),
-};
-
-module.exports = withNativeWind(config, { input: './app/globals.css' });
+module.exports = withNativeWind(config, { input: './app/globals.css' })
